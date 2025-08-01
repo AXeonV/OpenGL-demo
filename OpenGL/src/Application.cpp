9850eb6383465d624ece3c7fd57bbd4062e9475b
AXeonV <2607343351@qq.com>
2025-08-01 12:38:48 +0800

    use IBO to draw a rectangle

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -2,6 +2,37 @@
 #include <GLFW/glfw3.h>
 
 #include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+
+struct ShaderProgramSource {
+  std::string VertexSource;
+  std::string FragmentSource;
+};
+
+static ShaderProgramSource ParseShader(const std::string& filepath) {
+	std::ifstream stream(filepath);
+  std::string line;
+  std::stringstream ss[2];
+
+  enum class ShaderType {
+    NONE = -1, VERTEX = 0, FRAGMENT = 1
+	};
+	ShaderType type = ShaderType::NONE;
+
+  while (getline(stream, line)) {
+    if (line.find("#shader") != std::string::npos) {
+      if (line.find("vertex") != std::string::npos)
+        type = ShaderType::VERTEX;
+      else if (line.find("fragment") != std::string::npos)
+				type = ShaderType::FRAGMENT;
+    }
+    else ss[(int)type] << line << '\n';
+  }
+
+	return {ss[0].str(), ss[1].str()};
+}
 
 static unsigned int CompileShader(unsigned int type, const std::string& source) {
   unsigned int id = glCreateShader(type);
@@ -48,7 +79,7 @@ int main(void) {
     return -1;
 
   /* Create a windowed mode window and its OpenGL context */
-  window = glfwCreateWindow(1000, 1000, "Test", NULL, NULL);
+  window = glfwCreateWindow(1000, 750, "Test", NULL, NULL);
   if (!window) {
     glfwTerminate();
     return -1;
@@ -61,38 +92,33 @@ int main(void) {
     std::cout << "Error!" << std::endl;
   std::cout << glGetString(GL_VERSION) << std::endl;
 
-  float positions[6] = {
-    -0.5f, -0.5f,
-     0.0f,  0.5f,
-     0.5f, -0.5f,
+  float positions[] = {
+    -0.5f, -0.5f, // 0
+     0.5f, -0.5f, // 1
+		 0.5f,  0.5f, // 2
+		-0.5f,  0.5f  // 3
   };
 
+  unsigned int indices[] = {
+    0, 1, 2, // first triangle
+    2, 3, 0  // second triangle
+	};
+  
   unsigned int buffer; // the id of this buffer
   glGenBuffers(1, &buffer);
   glBindBuffer(GL_ARRAY_BUFFER, buffer);
-  glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
+  glBufferData(GL_ARRAY_BUFFER, 4 * 2 * sizeof(float), positions, GL_STATIC_DRAW);
 
   glEnableVertexAttribArray(0);
   glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), 0);
 
-  std::string VertexShader =
-    "#version 330 core\n"
-    "\n"
-    "layout(location = 0) in vec4 position;\n"
-    "\n"
-    "void main() {\n"
-    "gl_Position = position;"
-    "}\n";
-  std::string FragmentShader =
-    "#version 330 core\n"
-    "\n"
-    "layout(location = 0) out vec4 color;\n"
-    "\n"
-    "void main() {\n"
-    "color = vec4(0.5, 0.4, 0.9, 1.0);\n"
-    "}\n";
+	unsigned int ibo; // the id of this index buffer
+  glGenBuffers(1, &ibo);
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(unsigned int), indices, GL_STATIC_DRAW);
 
-  unsigned int shader = CreateShader(VertexShader, FragmentShader);
+	ShaderProgramSource source = ParseShader("res/shaders/Basic.shader");
+	unsigned int shader = CreateShader(source.VertexSource, source.FragmentSource);
 	glUseProgram(shader);
 
   /* Loop until the user closes the window */
@@ -100,7 +126,7 @@ int main(void) {
     /* Render here */
     glClear(GL_COLOR_BUFFER_BIT);
 
-    glDrawArrays(GL_TRIANGLES, 0, 3);
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
 
     /* Swap front and back buffers */
     glfwSwapBuffers(window);
@@ -109,7 +135,7 @@ int main(void) {
     glfwPollEvents();
   }
 
-  glDeleteShader(shader);
+  glDeleteProgram(shader);
 
   glfwTerminate();
   return 0;
--- README.md
+++ README.md
@@ -3,5 +3,6 @@ a demo for opengl learning
 
 1. create a window & draw a triangle (legacy OpenGL)
 2. use glew & test initialization
-3. learn buffer, vertex & draw a triangle (modern OpenGL)
+3. learn vertex buffer & draw a triangle (modern OpenGL)
 4. learn & write shader (create, compile & link) & colorfy the triangle
+5. redesign the file structure & learn index buffer object (IBO)
